654c9ee7600f4539ca4cc97d179c0388e1b7b096
Lowkeys101 <duseptyni@gmail.com>
2022-01-13 17:01:04 +0200

    Add html to display tic-tac-toe. Add pseudo code in js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Tic-tac-toe game, implemented with as little global code as possible 
+
+// TODO: Create Gameboard module to store gameboard state
+
+// TODO: Create Player modules
+
+// TODO: Create module to control gamestate
+
